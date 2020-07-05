5435e22fa599ad41dd4ce688345502a2a55e2db6
gplatono <gr.plat@gmail.com>
2020-07-05 15:35:23 -0400

    empty

--- bw-update.sh
+++ bw-update.sh
@@ -1,7 +1,9 @@
 #cd BlocksWorld
+echo Updating the main BW codebase...
 git stash
 git pull
 
+echo Updating the Eta dialog manager...
 cd ../eta
 git stash
 git pull
@@ -11,11 +13,18 @@ sed -i 's/\*perceptive-mode\* NIL/\*perceptive-mode\* T/g' config.lisp
 sed -i 's/\*responsive-mode\* NIL/\*responsive-mode\* T/g' config.lisp
 #cp -f ../config.lisp .
 
+echo Updating ulf2english...
 cd ../quicklisp/local-projects/ulf2english
 git pull
+
+echo Updating cl-util...
 cd ../cl-util
 git pull
+
+echo Updating ulf-lib...
 cd ../ulf-lib
 git pull
+
+echo Updating ttt...
 cd ../ttt
 git pull
